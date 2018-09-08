aa5b9134a443d6f3b19e8325e4f32e937ed15541
Dmitry <dimok-bocharov@mail.ru>
2018-09-08 18:41:38 +0300

    -

---
+++ Lesson_015/Lesson_015/ListWorker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_015
+{
+    internal class ListWorkers<Worker>
+    {
+        private List<Worker> list;
+
+        public List<Worker> ReadList { get; set; }
+
+        public int NumberOfWorkers
+        {
+            get { return list.Count; }
+        }
+
+        public ListWorkers()
+        {
+            list = new List<Worker>();
+        }
+
+        public ListWorkers(params Worker[] workers)
+        {
+            list = new List<Worker>();
+
+            if (workers.Length != 0)
+            {
+
+                for (int i = 0; i < workers.Length; i++)
+                {
+                    list.Add(workers[i]);
+                }
+            }
+        }
+
+        private void SortList()
+        {
+
+        }
+    }
+}
---
+++ Lesson_015/Lesson_015/Worker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_015
+{
+
+    internal struct Worker : IWorker
+    {
+        private string lName, name, fName;
+        private WorkPos workPos;
+        private int date;
+
+        public Worker(string lName, string name, string fName, WorkPos workPos)
+        {
+            this.lName = lName;
+            this.name = name;
+            this.fName = fName;
+            this.workPos = workPos;
+            date = DateTime.Today.Year;
+        }
+
+        public string LName
+        {
+            get { return name; }
+        }
+
+        public string Name
+        {
+            get { return lName; }
+        }
+
+        public string FName
+        {
+            get { return fName; }
+        }
+
+        public WorkPos WorkPos
+        {
+            get { return workPos; }
+        }
+
+        public int Date
+        {
+            get { return date; }
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Write()
+        {
+            Console.WriteLine($"Фамилия: {lName}\nИмя: {name}\nДолжность: {workPos}\nДата: {date}");
+        }
+
+        public void Change()
+        {
+        }
+    }
+}
